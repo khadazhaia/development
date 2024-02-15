5d9430f43e2b7d4a419c140e3f4ffb2773f70fc6
Khadazhaia Grayson <khadazhaiagrayson@Khadazhaias-Air.fios-router.home>
2024-02-14 19:38:30 -0500

    working through the code

---
+++ class 2/variables_operators.py
@@ -0,0 +1,132 @@
+# Print Built in Function
+
+# greeting = "Happy Wednesday"
+# print(greeting)
+
+# first_name = "Khadazhaia"
+# print(first_name)
+
+# Addition
+
+# print(4 + 2)
+# print(5 + 5)
+# print(100 + 5)
+
+# Subtraction
+
+# print(10 - 3)
+# print(4 - 3)
+# print(10 - 5)
+
+# Multiplication
+
+# print(3 * 3)
+# print(2 * 8)
+# print(5 * 7)
+
+# Division
+
+# print(10 / 2)
+# print(5 / 3)
+# print(5 / 0)
+
+# try:
+#     print(5 / 0)
+# except ZeroDivisionError as e:
+#     print("You cannot divide by zero, have a great day!")
+
+# Exponents
+print(5 ** 5)
+print(2 ** 5)
+print(3 ** 6)
+
+# Integer Division
+# print(5 // 2)
+# print(10 // 4)
+# print(6 // 2)
+
+# Modulo/Mod/Remainder
+# print(5 % 2)
+# print(10 % 4)
+# print(6 % 2)
+
+# Program to find the perimeter of a rectangle
+
+# length = 10
+# width = 7
+# perimeter = 2 * (length + width)
+# print("The perimeter of this rectangle is", perimeter)
+
+# Data Types
+
+
+# Integer
+# num_one = 5
+# print(num_one)
+# print(type(num_one))
+
+# String
+# fav_color = "blue"
+# print(fav_color)
+# print(type(fav_color))
+
+# Bool
+technical_errors = True
+print(technical_errors)
+print(type(technical_errors))
+
+# Float
+
+
+# String
+
+
+# List 
+
+
+# Dictionary
+
+
+# Cast a string to an integer
+
+
+
+# Cast integer to string
+
+
+# Student grade dataset
+
+
+# finding amount of assignments
+
+
+# Perimeter of a rectangle
+
+
+# Determine perimeter and display output
+
+
+# Fahrenheit to Celsius
+
+
+# Eval
+
+
+'''
+# You are given a triangle with a side #1 of 4, base of 6, and side #2 of 3. Create
+a brief python script that will determine the perimeter of the triangle. Comment your code
+
+1. Print the perimeter
+2. Using boolean operators is side #1 greater than the base?
+3. Using boolean operators is side #2 less than the base?
+4. Using boolean operators is base larger than or equal to side #1?
+'''
+
+
+
+
+
+
+
+
+
