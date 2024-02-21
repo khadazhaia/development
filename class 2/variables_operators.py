602eeee138d991622afe9827513494e4a982f8c8
Khadazhaia Grayson <khadazhaiagrayson@Khadazhaias-Air.fios-router.home>
2024-02-21 18:29:23 -0500

    working on shortcut operators

--- class 2/variables_operators.py
+++ class 2/variables_operators.py
@@ -153,18 +153,9 @@
 # print(celsius)
 
 # Eval
-cold_weather = "True"
-print(eval(cold_weather))
+# cold_weather = "True"
+# print(eval(cold_weather)) #eval turns a sting "True" or "False" into a Bool True of False
 
-'''
-# You are given a triangle with a side #1 of 4, base of 6, and side #2 of 3. Create
-a brief python script that will determine the perimeter of the triangle. Comment your code
-
-1. Print the perimeter
-2. Using boolean operators is side #1 greater than the base?
-3. Using boolean operators is side #2 less than the base?
-4. Using boolean operators is base larger than or equal to side #1?
-'''
 
 
 
---
+++ class 3/operators_bool_comments.py
@@ -0,0 +1,184 @@
+# Operators, Boolean Expressions, and Comments
+
+# I am a comment
+
+# Everything after the hash will be ignored when you run your code
+
+temperature = 95 # I can be an inline comment
+
+# Or I can be a standard single-line comment
+weekday = 'Wednesday'
+
+
+first_name = 'Thomas' ''' Multi-line comments can be inline '''
+last_name = 'The Train' """ This makes an inline comment as well """
+
+
+'''
+Will be a multi line comment 
+'''
+
+''' This will be inline '''
+
+"""
+Will be a multi line comment
+"""
+
+'''
+I am a multi-line comment and 
+can easily be used to span 
+multiple lines in a Python file
+'''
+
+"""
+You can also use the double quotes
+to create as a multi-line comment.
+Anything in between these three quotes will not run
+"""
+
+'''
+Comments that contradict the code are worse than no comments. Always make a priority of keeping the comments up-to-date when the code changes!
+'''
+
+'''
+Ensure that your comments are clear and easily understandable to other speakers of the language you are writing in.
+
+'''
+
+'''
+An inline comment is a comment on the same line as a statement. Inline comments should be separated by at least two spaces from the statement. They should start with a # and a single space.
+'''
+
+# Inline comments are unnecessary and in fact distracting if they state the obvious.
+
+# Comment me!
+
+# Find the perimeter of a rectangle 
+length = 5  
+width = 7 
+perimeter = 2 * (length + width)
+# print(perimeter)
+
+
+# Comment me!
+
+# Change Tuesday's temperature from Fahrenheight in to Celsius
+fahrenheight = 89 
+celsius = (fahrenheight - 32) * 5/9
+# print(celsius)
+
+
+'''Shortcut operators'''
+
+# Add 5 to me using shortcut operator +
+age = 25
+age += 5
+# print(age)
+
+# Add 10 to me using shortcut operator +
+year = 2024
+year += 10
+# print(year)
+
+# Subtract 20 -
+num_one = 55
+num_one -= 20
+# print(num_one)
+
+# Subtract 15 -
+num_two = 11
+num_two -= 15
+print(num_two)
+
+# Multiply by 3 *
+my_value = 9
+my_value *= 3
+
+# Multiply by 10 *
+mileage = 15
+
+
+# Divide by 2 /
+pizza_slices = 8
+
+
+# Divide by 7 /
+fees = 8.90
+
+
+# Raise to the 3rd power **
+num_three = 6
+
+
+
+# Raise to the 2nd power **
+data = 16
+
+
+# Integer divide by 3 //
+val_one = 16
+
+
+
+# Integer divide by 4 //
+val_two = 9
+
+
+# Find the remainder if divided by 3 %
+val_three = 10
+
+
+
+# Find the remainder if divided by 5 %
+val_four = 14
+
+
+# Refactor the fahrenheit/celsius converter using shortcut operators
+
+# fahrenheight = 89 
+# celsius = (fahrenheight - 32) * 5/9
+# print(celsius)
+
+
+''' Boolean Operators'''
+
+# Is 7 less than 5? <
+
+
+# Is 4 less than or equal to 4? <=
+
+
+# Is 6 greater than 2? >
+
+
+# Is 5 greater than or equal to 6? >=
+
+
+# Is 5 equal to 5? ==
+
+
+# Is 100 not equal to 75? !=
+
+
+# and
+
+
+# Values from example 1 can be stored in variables
+
+
+# or
+
+
+# not
+
+# Is x less than y?
+
+
+# Are we the same object? is 
+
+# in
+
+
+# eval
+
+
