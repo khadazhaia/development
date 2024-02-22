200160878d55fe4f132b094b9faad9cce50e9227
Khadazhaia Grayson <khadazhaiagrayson@Khadazhaias-Air.fios-router.home>
2024-02-21 19:02:39 -0500

    boolean operators

--- class 3/operators_bool_comments.py
+++ class 3/operators_bool_comments.py
@@ -119,42 +119,55 @@ num_three **= 3
 # Raise to the 2nd power **
 data = 16
 data **= 2
-print(data)
+# print(data)
 
 # Integer divide by 3 //
 val_one = 16
-
-
+val_one //= 3
+# print(val_one)
 
 # Integer divide by 4 //
 val_two = 9
-
+val_two //= 4
+# print(val_two)
 
 # Find the remainder if divided by 3 %
 val_three = 10
-
+val_three %= 3
+# print(val_three)
 
 
 # Find the remainder if divided by 5 %
 val_four = 14
-
+val_four %= 5
+# print(val_four)
 
 # Refactor the fahrenheit/celsius converter using shortcut operators
 
-# fahrenheight = 89 
-# celsius = (fahrenheight - 32) * 5/9
+fahrenheight = 89 
+fahrenheight -= 32
+fahrenheight *= 5/9
+celsius = fahrenheight
 # print(celsius)
 
 
 ''' Boolean Operators'''
 
 # Is 7 less than 5? <
-
+# print(7 < 5)
+result = (7 < 5)
+# print(result)
 
 # Is 4 less than or equal to 4? <=
-
+# print(4 <= 4)
+result = (4 <= 4)
+# print(result)
 
 # Is 6 greater than 2? >
+# print(6 > 2)
+result = (6 > 2)
+print(result)
+
 
 
 # Is 5 greater than or equal to 6? >=
