17edc6f1d7e828879928d93367c49e8f884e8cb8
Khadazhaia Grayson <khadazhaiagrayson@Khadazhaias-Air.fios-router.home>
2024-02-21 20:20:56 -0500

    assignment one

---
+++ class 3/assignment_one.py
@@ -0,0 +1,35 @@
+''' This program calculates the perimeter of a triangle
+
+Requirements
+    - Comment your code 
+        +5
+    - Assign meaningful variable names 
+        +5
+    - Output a print statement, "The perimeter of your triangle is..." 
+        +5
+    - The test case for your program is side # 1 is 5, the base is 6, side # 2 is 10 
+        +5
+    - Output a print statement, "Is side 1 greater than side 2?     
+        +5
+    - Output a print statement, "Is the base equal to side 1?"
+        +5
+'''
+
+
+# Assign meaningful variable names 
+side_one = 5
+base = 6
+side_two = 10
+
+# The formula for a perimeter of a triangle p = a + b + c
+perimeter = side_one + base + side_two
+
+# Output a print statement, "The perimeter of your triangle is...
+print("The perimeter of your triangle is", perimeter)
+
+# Output a print statement, "Is side 1 greater than side 2?          
+print(side_one > side_two)
+
+# Output a print statement, "Is the base equal to side 1?
+print(base == side_two)
+
