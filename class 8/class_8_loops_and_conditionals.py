b1a15324d2fea0fd3c831739af276c4f59c7be51
Khadazhaia Grayson <khadazhaiagrayson@Khadazhaias-Air.fios-router.home>
2024-03-11 20:11:24 -0400

    completed assignemnt

--- class 8/class_8_loops_and_conditionals.py
+++ class 8/class_8_loops_and_conditionals.py
@@ -256,7 +256,25 @@ new_string = " "
 while True:
     user_input = input("Please enter your data: ")
 
-    # if empty stop thw lopp
+    # if empty stop the lopp
     if len(user_input) == 0:
         print("String is empty, stopping the loop")
         break
+    # if number convert to float and add to total
+    elif user_input.isnumeric():
+        user_input = float(user_input) # casting to a float
+        new_total += user_input
+        print(f"Updated total to {new_total}")
+        continue
+    # if letter concatenate to new string
+    elif user_input.isalpha():
+        new_string += user_input
+        print(f"Your updated string is {new_string}")
+        continue
+    # Check for special characters
+    elif not user_input.isalnum():
+        print(f"{user_input} is a special character, no action and lets continue")
+        continue
+
+        
+    
