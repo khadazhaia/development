8c751fdf57cc41cfa33311c2bc56db34f4c69460
Khadazhaia Grayson <khadazhaiagrayson@Khadazhaias-Air.fios-router.home>
2024-03-21 21:30:00 -0400

    finished testing username. it works

--- project 1/project_one.py
+++ project 1/project_one.py
@@ -10,49 +10,39 @@ The username must start with a lowercase letter, and only contain letters, numbe
       
 The passowrd must be at least 8 characters long, contain at least one uppercase letter, contain at least one lowercase letter, contain at least one digit, contain at least one special character, and not have any spaces.\n''')
 
-user_name = " "
-pass_word = " "
+username, password = " ", " "
+
+reprompt_username, reprompt_password = " ", " "
 
 taken_usernames = ["admin", "admin123", "superuser", "superuser123"]
 
-error_messages = ["Username Taken", "Invalid Username", "Invalid Password"]
+error_message = ["Username Taken", "Invalid Username", "Invalid Password"]
 
-special_characters = ["!", "@", "#", "$", "%", "^", "&", "*"]
+special_character = ["!", "@", "#", "$", "%", "^", "&", "*"]
 
-# Get username and password from user. Compare user entries with system username and password. Do they match? (Initialize loop to prompt user, give error messages)
-
-# Not a taken username
+# Get username and password from user. Compare user entries with system username and password. Do they match? 
+   # Initialize loop to prompt user, Not a taken username, Give error messages
 while True:
    username = input("Please enter your username: ")
    username = username.strip()
+
+   password = input("Please enter your password: ")
+   password = password.strip()
   
    if username not in taken_usernames:
-       print(f"{username} is an available username")
+       pass
    else:
-       print(error_messages[0])
+       print(error_message[0])
        continue
    
-# Incorrect username or password (Testing username, Testing password)
-   
-   # starts wth a lowercase letter
-   starts_lower_case =  username.startswith(username.lower())
-   if starts_lower_case:
-       print(f"{username} starts with a lowercase letter")
+# Incorrect username or password  
+   # Testing Username: starts with a lowercase letter and only contain letters, numbers and underscore 
+   if   username[0].islower() and username.isidentifier():
+           print(f"Username: {username}")
    else:
-       print(f"{error_messages[1]}. Username must start with a lowercase letter")
-       continue
-        
-   # may only contain letters, numbers and underscore (this code not working yet)
-   if (username.isalnum() and "_" in username >= 0) or (username.isalpha and "_" in username >= 0):
-            print(f"{username} is a valid username")   
-   else:
-       print(f"{error_messages[1]}. Username may only contain letters, numbers and underscores")
+       print(f"{error_message[1]}. Username must start with a lowercase letter and only contain letters, numbers and underscores")
        continue
 
+   # Testing password: at least 8 characters long, contain at least one uppercase letter, contain at least one lowercase letter, contain at least one digit, contain at least one special character, and not have any spaces
 
-# if u.upper >= 1
-# if u.lower >= 1
-# if u.digit >= 1
-# if u.count(_) >= 1
-              
 # Login Successful! (Handling successful login)
