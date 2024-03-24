ec458204669149808635aa78d8f40f6e2b787126
Khadazhaia Grayson <khadazhaiagrayson@Khadazhaias-Air.fios-router.home>
2024-03-24 18:57:32 -0400

    project one complete

--- project 1/project_one.py
+++ project 1/project_one.py
@@ -8,7 +8,7 @@ print('''To sign up, enter a username and password.
       
 The username must start with a lowercase letter, and only contain letters, numbers, and underscores.
       
-The passowrd must be at least 8 characters long, contain at least one uppercase letter, contain at least one lowercase letter, contain at least one digit, contain at least one special character(!,?,@,#,$,^,&,*,_,-), and not have any spaces.\n''')
+The passowrd must be at least 8 characters long, contain at least one uppercase letter, contain at least one lowercase letter, contain at least one digit, contain at least one special character: !,?,@,#,$,^,&,*,_,-, and not have any spaces.\n''')
 
 username, password = " ", " "
 
@@ -53,7 +53,7 @@ while True:
    else:
        print(error_message[2],"\n")
        continue
-        
+          
 # Login Successful! 
    # Handling Successful Login: Reprompt user
 
