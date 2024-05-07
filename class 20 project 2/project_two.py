4b3f05ef5a30b8d399de4a71281823bdba60be8f
Khadazhaia Grayson <khadazhaiagrayson@Khadazhaias-Air.fios-router.home>
2024-05-06 21:14:10 -0400

    working on project 2

--- class 20 project 2/project_two.py
+++ class 20 project 2/project_two.py
@@ -9,37 +9,39 @@ The system allows the business to store and manage employee data, and perform ta
 class Employee:
     
      # intitalization, attributes (add accessor and mutator methods for each attribute so a user doesn't need to access them directly), type hinting, docstring
-    def  __init__(self, name: str, job_title: str, department: str, salary: float, hire_year: int):
-        self.name = name
-        self.job_title = job_title
-        self.department = department
-        self.salary = salary
-        self.hire_year = hire_year
+     def __init__(self, name: str, job_title: str, department: str, salary: float, hire_year: int):
+         self.name = name
+         self.job_title = job_title
+         self.department = department
+         self.salary = salary
+         self.hire_year = hire_year
 
      # __str__(): return a string representation
-    def __str__(self):
-        return f"{self.name} is a {self.job_title} in the {self.department} department who was hired in {self.hire_year} and makes {self.salary}"
+     def __str__(self):
+         return f"{self.name} is a {self.job_title} in the {self.department} department who was hired in {self.hire_year} and makes {self.salary}"
     
      # years_worked(): return the total years this employee has worked here, based on the hire year
-    def years_worked(self):
-        today = datetime.datetime.now()
-        year = today.year
-        return year - self.hire_year
+     def years_worked(self):
+         today = datetime.datetime.now()
+         year = today.year
+         return year - self.hire_year
     
      # total_expense(): calculate the total salary expense for this employee, which is the salary multiplied by the years worked  
-    def total_expense(self):
-        return self.salary * self.years_worked()
+     def total_expense(self):
+         return self.salary * self.years_worked()
   
      # write_employees(): Write your employee information to a text file
 
 employee1 = Employee("Zhaia", "Software Engineer", "Engineering", 125000, 2020)
 employee2 = Employee("Ally", "Software Engineer", "Engineering", 130000, 2020)
 employee3 = Employee("Nas", "Data Scientist", "Data", 125000, 2023)
-employee4 = Employee("Tye", "Accountant", "Finance",90000, 2021)
+employee4 = Employee("Tye", "Controller", "Finance",100000, 2021)
 employee5 = Employee("Zena", "Executive Assistant", "Operations", 90000, 2022)
-employee6 = Employee("Des","Human Resources Manager", "HR", 80000, 2021)
+employee6 = Employee("Des","Human Resources Manager", "HR", 85000, 2021)
     
+# testing
 print(employee1)
+
 print(employee1.years_worked())
     
 print(employee1.total_expense())
