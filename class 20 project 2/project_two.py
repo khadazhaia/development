487eb9ef10000055051272ad2757384916d49a33
Khadazhaia Grayson <khadazhaiagrayson@Khadazhaias-Air.fios-router.home>
2024-05-08 21:17:18 -0400

    project 2 working on mutator methods

--- class 20 project 2/project_two.py
+++ class 20 project 2/project_two.py
@@ -3,12 +3,17 @@ import datetime
 
 '''
 The system allows the business to store and manage employee data, and perform tasks related to employee management, such as adding and removing employees, updating employee information, searching for employees by name or department, and calculating total salary expenses.
+
+1.
+2.
+3.
+4.
 '''
 
 # creating employee class
 class Employee:
     
-     # intitalization, attributes (add accessor and mutator methods for each attribute so a user doesn't need to access them directly), type hinting, docstring
+     # intitalization, attributes, type hinting, docstring
      def __init__(self, name: str, job_title: str, department: str, salary: float, hire_year: int):
          self.name = name
          self.job_title = job_title
@@ -30,7 +35,39 @@ class Employee:
      def total_expense(self):
          return self.salary * self.years_worked()
   
-     # write_employees(): Write your employee information to a text file
+     # write_employees(): Write employee information to a text file
+     def write_employees(self):
+         f = open("employee_information.txt", "w")
+         f.write(f'''{self.name} is a {self.job_title} in the {self.department} department who was hired in {self.hire_year} and makes {self.salary:.02f}''')
+         f.close()
+    
+     # add accessor and mutator methods for each attribute so a user doesn't need to access them directly
+     
+     # Mutator method for name
+     def set_name(self, new_name):
+        self.name = new_name
+
+     # Mutator method for job_title
+     def set_job_title(self, new_job_title):
+        self.job_title = new_job_title
+
+     # Mutator method for department
+     def set_department(self, new_department):
+        self.department = new_department
+    
+     # Accessor method
+     # Accessor method for name (outputs name)
+     def get_name(self):
+        return self.name
+    
+     # Accessor method for job_title (outputs job_title)
+     def get_job_title(self):
+        return self.job_title
+     
+     # Accessor method for department (outputs department)
+     def get_department(self):
+        return self.department
+     
 
 employee1 = Employee("Zhaia", "Software Engineer", "Engineering", 125000, 2020)
 employee2 = Employee("Ally", "Software Engineer", "Engineering", 130000, 2020)
@@ -40,8 +77,12 @@ employee5 = Employee("Zena", "Executive Assistant", "Operations", 90000, 2022)
 employee6 = Employee("Des", "HR Manager", "HR", 85000, 2021)
     
 # testing
-print(employee1)
+# print(employee1)
 
-print(employee1.years_worked())
+# print(employee1.years_worked())
     
-print(employee1.total_expense())
+# print(employee1.total_expense())
+
+# employee1.write_employees()
+
+print(employee1.set_name("Amy"))
