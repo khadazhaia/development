d571f69d059aa2e6059f9ae49b920f3b364de044
Khadazhaia Grayson <khadazhaiagrayson@Khadazhaias-MacBook-Air.local>
2024-05-13 13:54:22 -0400

    project 2 finish testing all code working

--- class 20 project 2/project_two.py
+++ class 20 project 2/project_two.py
@@ -4,10 +4,12 @@ import datetime
 '''
 The system allows the business to store and manage employee data, and perform tasks related to employee management, such as adding and removing employees, updating employee information, searching for employees by name or department, and calculating total salary expenses.
 
-1.
-2.
-3.
-4.
+1. Create and initalize the employee class, will take 5 parameters
+2. Return a string representation
+3. Return the total years this employee has worked here, based on the hire year
+4. Calculate the total salary expense for this employee, which is the salary multiplied by the years worked  
+5. Write employee information to a text file
+6. Add accessor and mutator methods for each attribute so a user doesn't need to access them directly
 '''
 
 # creating employee class
@@ -43,41 +45,48 @@ class Employee:
     
      # add accessor and mutator methods for each attribute so a user doesn't need to access them directly
      
-     # Mutator method for name
+     # Accessor method 
+     def get_name(self):
+         return self.name
+    
+     def get_job_title(self):
+         return self.job_title
+
+     def get_department(self):
+         return self.department
+
+     def get_salary(self):
+         return self.salary 
+
+     def get_hire_year(self):
+         return self.hire_year
+     
+     # Mutator method
      def set_name(self, new_name):
         self.name = new_name
 
-     # Mutator method for job_title
      def set_job_title(self, new_job_title):
         self.job_title = new_job_title
 
-     # Mutator method for department
      def set_department(self, new_department):
         self.department = new_department
-    
-     # Accessor method
-     # Accessor method for name (outputs name)
-     def get_name(self):
-        return self.name
-    
-     # Accessor method for job_title (outputs job_title)
-     def get_job_title(self):
-        return self.job_title
-     
-     # Accessor method for department (outputs department)
-     def get_department(self):
-        return self.department
-     
+
+     def set_salary(self, new_salary):
+        self.salary = new_salary
+
+     def set_hire_year(self, new_hire_year):
+        self.hire_year = new_hire_year
+   
 
 employee1 = Employee("Zhaia", "Software Engineer", "Engineering", 125000, 2020)
-employee2 = Employee("Ally", "Software Engineer", "Engineering", 130000, 2020)
-employee3 = Employee("Nas", "Data Scientist", "Data", 125000, 2023)
-employee4 = Employee("Tye", "Controller", "Finance",100000, 2021)
-employee5 = Employee("Zena", "Executive Assistant", "Operations", 90000, 2022)
-employee6 = Employee("Des", "HR Manager", "HR", 85000, 2021)
+# employee2 = Employee("Ally", "Software Engineer", "Engineering", 130000, 2020)
+# employee3 = Employee("Nas", "Data Scientist", "Data", 125000, 2023)
+# employee4 = Employee("Tye", "Controller", "Finance",100000, 2021)
+# employee5 = Employee("Zena", "Executive Assistant", "Operations", 90000, 2022)
+# employee6 = Employee("Des", "HR Manager", "HR", 85000, 2021)
     
 # testing
-# print(employee1)
+print(employee1)
 
 # print(employee1.years_worked())
     
@@ -85,4 +94,32 @@ employee6 = Employee("Des", "HR Manager", "HR", 85000, 2021)
 
 # employee1.write_employees()
 
-print(employee1.set_name("Amy"))
+# Accessor method
+print(employee1.get_name())
+print(employee1.get_job_title())
+print(employee1.get_department())
+print(employee1.get_salary())
+print(employee1.get_hire_year())
+
+# Mutator method 
+employee1.set_name("Amy")
+print(employee1)
+
+employee1.set_job_title("Painter")
+print(employee1)
+
+employee1.set_department("Architecture")
+print(employee1)
+
+employee1.set_salary(120000)
+print(employee1)
+
+employee1.set_hire_year(2018)
+print(employee1)
+
+# Accessor method
+print(employee1.get_name())
+print(employee1.get_job_title())
+print(employee1.get_department())
+print(employee1.get_salary())
+print(employee1.get_hire_year())
